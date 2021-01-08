e51e6c652d531dee1f0164cdec94a24703446da1
ufukcanli <iletisim@ufukcanli.com>
2021-01-08 17:03:25 +0300

    Layout cell.

--- AppStore/SearchResultCell.swift
+++ AppStore/SearchResultCell.swift
@@ -11,11 +11,15 @@ final class SearchResultCell: UICollectionViewCell {
     
     static let reuseIdentifier = "SearchResultCell"
     
-    private let imageView = UIImageView()
+    private let appIconImageView = UIImageView()
     private let nameLabel = UILabel()
     private let categoryLabel = UILabel()
     private let ratingsLabel = UILabel()
     private let getButton = UIButton(type: .system)
+    private lazy var screenShotImageView1 = self.createScreenshotImageView()
+    private lazy var screenShotImageView2 = self.createScreenshotImageView()
+    private lazy var screenShotImageView3 = self.createScreenshotImageView()
+
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -27,42 +31,70 @@ final class SearchResultCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func configureCell() {
-        backgroundColor = .systemGreen
-        
-        imageView.backgroundColor = .systemRed
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        
+    private func configureAppIconImageView() {
+        appIconImageView.backgroundColor = .systemRed
+        appIconImageView.layer.cornerRadius = 12
+        appIconImageView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func configureLabels() {
         nameLabel.text = "APP NAME"
         categoryLabel.text = "Photos & Video"
         ratingsLabel.text = "9.26M"
-        
-        let labelStackView = UIStackView(arrangedSubviews: [nameLabel, categoryLabel, ratingsLabel])
-        labelStackView.axis = .vertical
-        
+    }
+    
+    private func configureGetButton() {
         getButton.backgroundColor = .systemGray
         getButton.setTitle("GET", for: .normal)
+        getButton.layer.cornerRadius = 16
         getButton.setTitleColor(.systemBlue, for: .normal)
         getButton.titleLabel?.font = .boldSystemFont(ofSize: 14)
         getButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        let containerStackView = UIStackView(arrangedSubviews: [imageView, labelStackView, getButton])
-        containerStackView.translatesAutoresizingMaskIntoConstraints = false
-        containerStackView.alignment = .center
-        containerStackView.spacing = 12
+    }
     
+    private func createScreenshotImageView() -> UIImageView {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .systemBlue
+        return imageView
+    }
+    
+    private func configureCell() {
+        backgroundColor = .systemGreen
+        
+        configureAppIconImageView()
+        
+        configureLabels()
+        let labelStackView = UIStackView(arrangedSubviews: [nameLabel, categoryLabel, ratingsLabel])
+        labelStackView.axis = .vertical
+        
+        configureGetButton()
+        
+        let infoStackView = UIStackView(arrangedSubviews: [appIconImageView, labelStackView, getButton])
+        infoStackView.alignment = .center
+        infoStackView.spacing = 12
+        
+        let screenshotStackView = UIStackView(arrangedSubviews: [screenShotImageView1, screenShotImageView2, screenShotImageView3])
+        screenshotStackView.spacing = 10
+        screenshotStackView.distribution = .fillEqually
+        
+        let containerStackView = UIStackView(arrangedSubviews: [infoStackView, screenshotStackView])
+        containerStackView.translatesAutoresizingMaskIntoConstraints = false
+        containerStackView.axis = .vertical
+        containerStackView.spacing = 12
+        
         addSubview(containerStackView)
         
         NSLayoutConstraint.activate([
-            imageView.widthAnchor.constraint(equalToConstant: 64),
-            imageView.heightAnchor.constraint(equalToConstant: 64),
+            appIconImageView.widthAnchor.constraint(equalToConstant: 64),
+            appIconImageView.heightAnchor.constraint(equalToConstant: 64),
             
             getButton.widthAnchor.constraint(equalToConstant: 80),
+            getButton.heightAnchor.constraint(equalToConstant: 32),
             
-            containerStackView.topAnchor.constraint(equalTo: self.topAnchor),
-            containerStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            containerStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-            containerStackView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+            containerStackView.topAnchor.constraint(equalTo: self.layoutMarginsGuide.topAnchor),
+            containerStackView.trailingAnchor.constraint(equalTo: self.layoutMarginsGuide.trailingAnchor),
+            containerStackView.leadingAnchor.constraint(equalTo: self.layoutMarginsGuide.leadingAnchor),
+            containerStackView.bottomAnchor.constraint(equalTo: self.layoutMarginsGuide.bottomAnchor)
         ])
     }
     
