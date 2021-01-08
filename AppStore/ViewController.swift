b3aa0e789953e353b990cbee84b874bb83d37f48
ufukcanli <iletisim@ufukcanli.com>
2021-01-08 13:48:47 +0300

    Initial Commit

---
+++ AppStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStore
+//
+//  Created by Ufuk Canlı on 8.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
